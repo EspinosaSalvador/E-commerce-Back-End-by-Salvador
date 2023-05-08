59a74838e835e4fd05b9e09433bdde9b2d03c3b6
Salvador Espinosa <espinosasalvador94@outlook.com>
2023-05-08 15:49:12 -0600

    finised with js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-  onDelete: "cascade",
+  // onDelete: "cascade",
 });
 // Categories have many Products
 Category.hasMany(Product, {
